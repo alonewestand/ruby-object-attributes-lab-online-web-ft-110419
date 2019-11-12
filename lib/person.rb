4f4d0854a8e2ce9e013ceba6a6655cc315eb2130
Ken Goad <goadke@g.cofc.edu>
2019-11-12 05:01:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,12 +8,12 @@ def name=(man_name)
   @name = man_name
 end
 
-def walk
-  @walk
+def job
+  @job
 end
 
-def walk=(man_walk)
-  @walk = man_walk
+def job=(mans_job)
+  @job = mans_job
 end
 
 end
